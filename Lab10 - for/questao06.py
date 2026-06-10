4876fdcbf1d7b2a9b65181b6e72854a252502ed6
Luan Resende Ferreira <luanjenner@gmail.com>
2026-06-10 14:46:19 -0300

    prontim

--- Lab10 - for/questao06.py
+++ Lab10 - for/questao06.py
@@ -1,4 +1,4 @@
 numero = int(input("Digite um número entre 1 e 10: "))
 
 for i in range(1, 11):
-    print(f"{numero} X {i} = {numero * i}")
+    print(f"{numero} x {i} = {numero * i}")
--- Lab10 - for/questao07.py
+++ Lab10 - for/questao07.py
@@ -1,24 +1,24 @@
-jogos = int(input("Quantidade de jogos: "))
-
 vitorias = 0
 empates = 0
 derrotas = 0
 pontos = 0
 
+jogos = int(input("Quantidade de jogos: "))
+
 for i in range(jogos):
     gols_galo = int(input("Gols do Galo: "))
-    gols_oponente = int(input("Gols do adversário: "))
+    gols_adversario = int(input("Gols do adversário: "))
 
-    if gols_galo > gols_oponente:
+    if gols_galo > gols_adversario:
         vitorias += 1
         pontos += 3
-    elif gols_galo == gols_oponente:
+    elif gols_galo == gols_adversario:
         empates += 1
         pontos += 1
     else:
         derrotas += 1
 
-print(f"Vitórias: {vitorias}")
-print(f"Empates: {empates}")
-print(f"Derrotas: {derrotas}")
-print(f"Pontuação: {pontos}")
+print(f"O galo venceu {vitorias} partidas")
+print(f"O galo empatou {empates} partidas")
+print(f"O galo perdeu {derrotas} partidas")
+print(f"O galo fez {pontos} pontos")
