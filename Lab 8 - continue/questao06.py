008cd3c8c60ea8e87678036ab0e842fd29065e70
Luan Resende Ferreira <luanjenner@gmail.com>
2026-06-10 14:49:42 -0300

    atualizacao final

--- Lab 8 - continue/questao06.py
+++ Lab 8 - continue/questao06.py
@@ -5,13 +5,28 @@ while True:
         print("Não é possível realizar esse saque.")
         continue
 
-    if valor % 10 != 0:
-        print("Não é possível realizar esse saque.")
-        continue
+    possivel = False
+    nota100 = 0
 
-    if valor in [30, 70]:
-        print("Não é possível realizar esse saque.")
-        continue
+    while nota100 * 100 <= valor:
+        nota50 = 0
 
-    print("Saque realizado com sucesso!")
-    break
+        while nota100 * 100 + nota50 * 50 <= valor:
+            restante = valor - (nota100 * 100 + nota50 * 50)
+
+            if restante % 20 == 0:
+                possivel = True
+                break
+
+            nota50 += 1
+
+        if possivel:
+            break
+
+        nota100 += 1
+
+    if possivel:
+        print("Saque realizado com sucesso!")
+        break
+    else:
+        print("Não é possível realizar esse saque.")
